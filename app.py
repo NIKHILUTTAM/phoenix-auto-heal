2c63c4c2b2cfb69bdf812874b5c4252acf1d8c0f
NIKHILUTTAM <153156217+NIKHILUTTAM@users.noreply.github.com>
2025-11-23 10:03:23 +0530

    Testing Discord Alert

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def power_level():
     # A simple function that returns the system power level
-    return 100
+    return 0
 
 if __name__ == "__main__":
     print(f"System Power Level: {power_level()}%")
