ae5ab7a16225cc97f5fc073d2e9542e3cdb749f2
NIKHILUTTAM <153156217+NIKHILUTTAM@users.noreply.github.com>
2025-11-23 08:34:34 +0530

    Deploying bad update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def power_level():
     # A simple function that returns the system power level
-    return 100
+    return 50
 
 if __name__ == "__main__":
     print(f"System Power Level: {power_level()}%")
